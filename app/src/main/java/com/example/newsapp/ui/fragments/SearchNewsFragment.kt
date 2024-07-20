fa784f9a21e4f34fc488a46397864e3eeeaac5f0
Anwar Zahid <anwarzahid8284>
2024-07-20 12:37:55 +0500

    initial commit

---
+++ app/src/main/java/com/example/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,22 @@
+package com.example.newsapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.newsapp.R
+
+class SearchNewsFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search_news, container, false)
+    }
+}
