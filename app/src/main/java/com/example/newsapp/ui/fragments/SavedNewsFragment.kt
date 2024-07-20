44ff062da7e1e67a7cd76d0532a9f13da379ed3b
Anwar Zahid <anwarzahid8284>
2024-07-20 12:40:01 +0500

    news app project added

--- app/src/main/java/com/example/newsapp/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/fragments/SavedNewsFragment.kt
@@ -1,17 +1,13 @@
 package com.example.newsapp.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.newsapp.R
 
 class SavedNewsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
